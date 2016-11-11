21cc781300a25e87908825849f9d8fae02edf626
Isaac Halvorson <hello@hisaac.net>
2016-11-11 16:28:26 -0600

    build form

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 //---------------------------------- SETUP -----------------------------------//
 
 var express = require('express');
-var index = require('./routes/index');
 var bodyParser = require('body-parser');
+var index = require('./routes/index');
 var app = express();
 var port = 8000;
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path');
 
 router.use('/', function(req, res){
-  res.sendFile(path.resolve('./server/public/index.html'));
+  res.sendFile(path.resolve('./public/views/index.html'));
 });
 
 module.exports = router;
